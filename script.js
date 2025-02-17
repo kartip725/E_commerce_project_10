ae3bfeaf4745321facfe13801aab21000a62718e
kartip725 <patilaishu268@gmail.com>
2025-02-17 14:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//add EventListner to button
+document.getElementById("button")?.addEventListener("click",() =>{
+    const query = document.getElementById("Register here").Value;
+    RegisterHere(query);
+})
